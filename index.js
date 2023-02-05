316cee8857211b6e4af5ada9f5f8619d323fcf33
Leehuseung <gntmd4261@naver.com>
2023-02-05 17:07:17 +0900

    Player movement implement

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ class Player {
 
     update() {
         this.draw()
+
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity
         else this.velocity.y = 0
     }
@@ -37,12 +40,54 @@ const player2 = new Player({
     y: 100,
 });
 
+
+const keys = {
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     player2.update()
+
+    player.velocity.x = 0
+    if(keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            player.velocity.x = 1
+            break
+        case 'a':
+            keys.a.pressed = true;
+            player.velocity.x = -1
+            break
+        case 'w':
+            player.velocity.y = -20
+            break
+    }
+})
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            player.velocity.x = 1
+            break
+        case 'a':
+            keys.a.pressed = false
+            player.velocity.x = -1
+            break
+    }
+})
